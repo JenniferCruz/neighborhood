ea9afe6abd1448839bc6586c91a76c05823f5f8b
jennifer <jennifer.cruzz@gmail.com>
2017-01-30 21:04:25 -0600

    feat: Display a list and support filtering and selection of elements with KO

--- js/app.js
+++ js/app.js
@@ -1,54 +1,70 @@
-var map;
+// PLAIN INFO
+var cityLocations = [];
+var categories = ['all', 'urban sight', 'street', 'route', 'architecture', 'food', 'store', 'museum', 'things to do', 'restaurant', 'dance', 'salsa', 'son', 'nightlife'];
+cityLocations.push({
+    name : 'Calle Las Damas',
+    location : {lat: 18.47317, lng: -69.882598},
+    tags : [categories[1], categories[2], categories[3], categories[0]]
+});
+cityLocations.push({
+    name : 'Chocomuseo',
+    location : {lat: 18.473665, lng: -69.884687},
+    tags : [categories[5], categories[6], categories[7],categories[8], categories[0]]
+});
+cityLocations.push({
+    name : 'La cafetera',
+    location : {lat : 18.473219, lng : -69.886549},
+    tags : [categories[9], categories[5], categories[0]]
+});
+cityLocations.push({
+    name : 'El bar de Lucia',
+    location : {lat : 18.473889, lng : -69.88527},
+    tags : [categories[5], categories[9], categories[10], categories[11], categories[12], categories[13], categories[0]]
+});
+cityLocations.push({
+    name : 'Jalao\'',
+    location : {lat : 18.473827, lng : -69.883973},
+    tags : [categories[5], categories[9], categories[0]]
+});
 
-var loadMap = function () {
-    var markers = [];
-
-    // TODO: Load a 'Welcome to Santo Domingo" banner ontop of map, which disappears in a few seconds
-    map = new google.maps.Map(document.getElementById('map'), {
-        center : {lat: 18.4726498, lng: -69.8865431},
-        zoom : 17
-    });
-
-    for(var i = 0; i < locations.length; i++) {
-        var mark = new google.maps.Marker({
-                position : locations[i].location,
-                map : map,
-                title : locations[i].place});
-        markers.push(mark);
-        mark.addListener('click', function(){
-            highlight(this);
-        });
-    }
-
-};
-
-var highlight = function() {
-
-};
-
-var Location = function(place) {
-    // var self = this;
-    this.title = ko.observable(place.name);
-    // this.coords = ko.observable(place.location);
-    // this.tags = ko.observableArray([]);
-    // place.tags.forEach(function(tag){
-       // self.tags.push(ko.observable(tag));
-    // });
-    return this;
-};
-
-var PlacesViewModel = function() {
+// OBJECT - Seems unnecessary - might need to DRY it out... let's see how this evolves..
+var Location = function(data) {
     var self = this;
-    self.locations = ko.observableArray([]);
-    cityLocations.forEach(function(place) {
-        var p = ko.observable(new Location(place));
-        self.locations.push(p);
-    });
-
-    var highlightItem = function(place) {
-        console.log('!');
-        console.log(place);
-    }
+    self.name = data.name;
+    self.tags = data.tags;
+    self.visible = ko.observable(true);
+    return self;
 };
 
-ko.applyBindings(new PlacesViewModel());
+// VIEW MODEL
+var LocationsViewModel = function() {
+    // DATA
+    var self = this;
+    self.locations = new ko.observableArray([]);
+    cityLocations.forEach(function(location){self.locations.push(new Location(location));});
+
+    self.tags = ko.observableArray(categories);
+    self.selectedFilter = ko.observable(self.tags[3]);
+    self.selectedLocation = ko.observable(undefined);
+
+    // BEHAVIOUR
+    self.select = function(location) {
+        if (location === self.selectedLocation())
+            self.selectedLocation('undefined');
+        else
+            self.selectedLocation(location);
+    };
+
+    self.filter = function(vm, event) {
+        var filterTag = event.srcElement.value;
+        for(var i = 0; i < self.locations().length; i++) {
+            var currentLocation = self.locations()[i];
+            if (currentLocation.tags.includes(filterTag))
+                currentLocation.visible(true);
+            else currentLocation.visible(false);
+        }
+    };
+};
+
+
+ko.applyBindings(new LocationsViewModel());
